37e7e0bb0eef540c2390f009817ca069ebe0ac51
Steven Majek <majek1@hotmail.com>
2022-08-31 14:10:06 +0200

    Correct login api path

--- client/src/redux/saga/request/login.ts
+++ client/src/redux/saga/request/login.ts
@@ -3,7 +3,7 @@ import { ApiResponse } from "./../../../shared/models/api";
 import { axiosInstance } from "../../../utils/axios";
 
 export function loginSagaRequest(loginInput: LoginInput) {
-  return axiosInstance().post<ApiResponse<string>>("/auth/login", {
+  return axiosInstance().post<ApiResponse<string>>("/api/auth/login", {
     ...loginInput,
   });
 }
