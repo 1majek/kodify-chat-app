ea6c93924fec59cdef2f736949a95f976612c63b
Steven Majek <majek1@hotmail.com>
2022-09-02 08:11:04 +0200

    Add uuid to fix key error

--- client/src/components/MessageDisplay.tsx
+++ client/src/components/MessageDisplay.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { Message, User } from "../shared/models";
 import styles from "./message.module.css";
 import classNames from "./../../node_modules/classnames/dedupe.d";
+import { v4 as uuidv4 } from "uuid";
 
 interface Props {
   user: User | null;
@@ -23,11 +24,7 @@ const MessageDisplay: FC<Props> = ({ user, messages, messageContentRef }) => {
   return (
     <div className={styles.messageContent}>
       {messages.map((message) => (
-        <div
-          ref={messageContentRef}
-          key={message.id}
-          className={styles.message}
-        >
+        <div ref={messageContentRef} key={uuidv4()} className={styles.message}>
           <div className={chatStyle(message.userId)}>
             <span>{message.content}</span>
           </div>
